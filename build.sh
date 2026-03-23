3ab79f03fc711572f1e7b7a723b740d37387a2bd
Aurentide <jacksontanna@proton.me>
2026-03-23 15:53:47 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sh build/mkimage.sh --tag edge \
+    --outdir ~/iso \
+    --arch x86_64 \
+    --repository https://dl-cdn.alpinelinux.org/alpine/edge/main \
+    --extra-repository https://dl-cdn.alpinelinux.org/alpine/edge/community \
+    --profile bastion
