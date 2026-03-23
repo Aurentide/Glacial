a9bc08b1299bd07355a92b9eb2d12da3a5b17713
Aurentide <jacksontanna@proton.me>
2026-03-23 15:51:46 -0400

    Delete build/build.sh

--- build/build.sh
+++
@@ -1,6 +0,0 @@
-sh ./mkimage.sh --tag edge \
-        --outdir ~/iso \
-        --arch x86_64 \
-        --repository https://dl-cdn.alpinelinux.org/alpine/edge/main \
-        --repository https://dl-cdn.alpinelinux.org/alpine/edge/community \
-        --profile bastion
